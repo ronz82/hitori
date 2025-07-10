c91aa096dc19e78bfa989692a3fcda594610cf9e
Ronz <xyronz94@gmail.com>
2025-07-11 04:05:42 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,10 +9,10 @@ const chalk = require('chalk');
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188'] //['628','628'] 2 owner atau lebih
+global.owner = ['6285148364728,6288227905197'] //['628','628'] 2 owner atau lebih
 global.packname = 'Bot WhatsApp'
-global.author = 'Nazedev'
-global.botname = 'Naze Bot'
+global.author = '𝐑𝐎𝐍𝐙 𝐒𝐍𝐀𝐊𝐄𝐇𝐄𝐀𝐃 𝐌𝐀𝐆𝐄𝐋𝐀𝐍𝐆 '
+global.botname = '𝐑𝐎𝐍𝐙 𝐒𝐍𝐀𝐊𝐄𝐇𝐄𝐀𝐃 𝐌𝐀𝐆𝐄𝐋𝐀𝐍𝐆 '
 global.listprefix = ['+','!','.']
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
 global.tempatDB = 'database.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
@@ -21,17 +21,17 @@ global.pairing_code = true
 global.number_bot = '' // Kalo pake panel bisa masukin nomer di sini, jika belum ambil session. Format : '628xx'
 
 global.fake = {
-	anonim: 'https://telegra.ph/file/95670d63378f7f4210f03.png',
-	thumbnailUrl: 'https://telegra.ph/file/fe4843a1261fc414542c4.jpg',
-	thumbnail: fs.readFileSync('./src/media/naze.png'),
+	anonim: 'https://n.uguu.se/ZiePwUqi.jpg',
+	thumbnailUrl: 'https://n.uguu.se/ZiePwUqi.jpg',
+	thumbnail: fs.readFileSync('./src/media/ronz.png'),
 	docs: fs.readFileSync('./src/media/fake.pdf'),
 	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
 }
 
 global.my = {
-	yt: 'https://youtube.com/c/Nazedev',
-	gh: 'https://github.com/nazedev',
-	gc: 'https://chat.whatsapp.com/B5qJIwZHm4VEYZJQE6iMwy',
+	yt: 'https://youtube.com/c/ronz Snakehead Magelang',
+	gh: 'https://github.com/ronz82',
+	gc: 'https://chat.whatsapp.com/LhouPjGCyhDJfJj4aDOTU5?mode=ac_t',
 	ch: '120363250409960161@newsletter',
 }
 
@@ -48,7 +48,7 @@ global.money = {
 }
 
 global.mess = {
-	key: 'Apikey mu telah habis silahkan kunjungi\nhttps://my.hitori.pw',
+	key: 'Apikey mu telah habis silahkan kunjungi\nhttps://',
 	owner: 'Fitur Khusus Owner!',
 	admin: 'Fitur Khusus Admin!',
 	botAdmin: 'Bot Bukan Admin!',
@@ -83,3 +83,4 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 });
+	
